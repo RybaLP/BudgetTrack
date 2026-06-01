83f73e0473d7eb1b60663aff925523de127879ec
RybaLP <filip9999@onet.pl>
2026-06-01 11:47:50 +0200

    typo fix

--- backend/src/main/java/com/filip/budgetapp/account/AccountService.java
+++ backend/src/main/java/com/filip/budgetapp/account/AccountService.java
@@ -6,7 +6,7 @@ import com.filip.budgetapp.transaction.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional; // Najlepiej używać importu ze Springa
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
